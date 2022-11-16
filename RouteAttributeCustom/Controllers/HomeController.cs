ed4476de5a344ffa67d9bb7b5b51343afdf0ee6f
Surya.99 <surya@nimapinfotech.com>
2022-11-16 17:29:08 +0530

    Route Attribute

---
+++ RouteAttributeCustom/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace RouteAttributeCustom.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: RouteAttribute
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("routeattri/released/{year}/{month}")]
+        public ActionResult ByReleaseYear()
+        {
+            return View();
+        }
+    }
+}
