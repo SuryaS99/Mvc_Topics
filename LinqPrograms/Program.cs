0b85be943c09ddce9a297f9a3c77cc94753fe468
Surya.99 <surya@nimapinfotech.com>
2022-11-17 10:41:07 +0530

    Linq Program

---
+++ LinqPrograms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Array
+            int[] age = { 22, 34, 54, 21, 23, 63, 43, 41 };
+            //i is a temproray name to strore data
+            //a is a collecion
+            //linq query from with select to display data
+            Console.WriteLine("\n linq query from with select to display data");
+            var a = from i in age where i < 60 select i;//linq query
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            //linq query from with orderby (by default acesending)
+            Console.WriteLine("\n linq query from with orderby (by default acesending)");
+            var b = from i in age where i < 60 orderby i select i;//linq query
+            foreach (var item in b)
+            {
+                Console.WriteLine(item);
+            }
+
+            //linq query from with orderby (by descending)
+            Console.WriteLine("\n linq query from with orderby (by descending)");
+            var c = from i in age where i < 60 orderby i descending select i;//linq query
+
+            foreach (var item in c)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LinqQuery/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string array
+            string[] names = { "Ali", "Dipak", "Nisha", "Garima", "Imran", "Sneha", "Firoz", "Sahnu", "Disha" };
+            //contains is used to check the character in the object
+            var a = from name in names where name.Contains("a") select name;
+
+            // we can use var or string
+            foreach (string item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
